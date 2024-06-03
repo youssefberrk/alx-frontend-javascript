9cb8c3f45898f7ee76de3127d769a7ca6d3b9e93
youssef <135049429+youssefberrk@users.noreply.github.com>
2024-06-03 14:14:48 +0100

    Done

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(studentList) {
+    if (Array.isArray(studentList)) {
+      const studentIds = studentList.map((student) => student.id);
+      return studentIds;
+    }
+    return [];
+  }
+  
