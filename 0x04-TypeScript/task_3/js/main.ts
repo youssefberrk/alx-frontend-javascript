2564a8ee616389e28b6020cd5bab6e6ce72842ff
youssef <yberrakouan.lol@gmail.com>
2024-12-11 11:39:33 +0100

    Ambient Namespaces

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,15 +1,22 @@
-/// <reference path="./crud.d.ts" />
+/// <reference path="./crud.d.ts" />  // Reference to type definitions for CRUD operations
 
+// Import required types and CRUD module
 import { RowID, RowElement } from "./interface";
 import * as CRUD from "./crud";
 
+// Create a new row element with initial data
 const row: RowElement = {
-  firstName: "Guillaume",
-  lastName: "Salva",
+  firstName: "Guillaume",  // First name of the row element
+  lastName: "Salva",       // Last name of the row element
 };
 
+// Insert the row into the data source and store its unique ID
 const newRowID: RowID = CRUD.insertRow(row);
-const UpdatedRow: RowElement = { age: 23, ...row };
 
-CRUD.updateRow(newRowID, UpdatedRow);
+// Update the row by adding a new property `age` and keeping existing data
+const UpdatedRow: RowElement = { age: 23, ...row };
+CRUD.updateRow(newRowID, UpdatedRow);  // Update the row in the data source
+
+// Delete the row using its unique ID
 CRUD.deleteRow(newRowID);
+
