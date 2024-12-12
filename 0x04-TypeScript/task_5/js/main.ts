92b7c4e0848538a7515f0c0df747361cf9c94642
youssef <yberrakouan.lol@gmail.com>
2024-12-12 12:24:12 +0100

    Brand convention & Nominal typing

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,17 +1,21 @@
+// Defining an interface for MajorCredits with a unique brand property
 interface MajorCredits {
-  credits: number;
-  brand: "major";
+  credits: number;  // Number of credits for a major subject
+  brand: "major";   // Unique identifier to distinguish from minor credits
 }
 
+// Defining an interface for MinorCredits with a unique brand property
 interface MinorCredits {
-  credits: number;
-  brand: "minor";
+  credits: number;  // Number of credits for a minor subject
+  brand: "minor";   // Unique identifier to distinguish from major credits
 }
 
+// Function to sum credits for major subjects
 function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
-  return subject1.credits + subject2.credits;
+  return subject1.credits + subject2.credits;  // Summing the credits of two major subjects
 }
 
+// Function to sum credits for minor subjects
 function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
-  return subject1.credits + subject2.credits;
+  return subject1.credits + subject2.credits;  // Summing the credits of two minor subjects
 }
